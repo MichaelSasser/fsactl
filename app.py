f3c4c934ae0c41b8b05ae95c5acd13b0aa5e5739
Michael Sasser <Michael@MichaelSasser.org>
2020-11-20 17:09:44 +0100

    Update CRLF to LF, Dependencies

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# fsactl - install and manager your MSFS2020 addons
-# Copyright (c) 2020  Michael Sasser <Michael@MichaelSasser.org>
+#  ${PROJECT_NAME} - install and manage your MSFS2020 addons
+#  Copyright (C) ${YEAR}  Michael Sasser <Michael@MichaelSasser.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
